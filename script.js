39e25b7b3cd9b6eed9dac78ae58a4b838365da17
NewMicRob <microbnew@gmail.com>
2025-04-01 01:13:16 -0400

    added some style, added bew fields to html, added form mouse and keyboard functionality with javscript

---
+++ script.js
@@ -0,0 +1,45 @@
+//Mouse Event 
+document.getElementById('mouse-event').addEventListener('click', function() {
+    alert('Mouse Event Listener Engaged');
+});
+
+document.addEventListener('keydown', function(event) {
+    console.log(`keyboard input: ${event.key}`);
+    if (event.key === 'Escape') {
+        console.log('Escape Key was Pressed');
+    }
+});
+
+//Form
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('theForm');
+    const fName = document.getElementById('fName');
+    const lName = document.getElementById('lName');
+    const number = document.getElementById('number');
+    
+    form.addEventListener('submit', function(event) {
+        console.log("submitted");
+        event.preventDefault();
+        alert(`First name is ${fName.value}, last name is ${lName.value}, the number selected is ${number.value}`);
+    });
+
+    number.addEventListener('input', function() {
+        console.log('Number chosen changed to: ', this.value);
+    });
+
+    fName.addEventListener('change', function() {
+        console.log('First name has been changed to: ', this.value);
+    });
+
+    lName.addEventListener('change', function() {
+        console.log('Last name has been changed to: ', this.value);
+    });
+
+    fName.addEventListener('input', function() {
+        console.log('First name input changed: ', this.value);
+    });
+
+    lName.addEventListener('input', function() {
+        console.log('Last name input changed: ', this.value);
+    });
+});
